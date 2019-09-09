d5495f759942c1912005c6563c0ba08a435aad75
kang <khsok212@gmail.com>
2019-09-09 12:25:25 +0900

    mvc 프레임워크

---
+++ src/test/java/kr/or/ddit/mvc/repository/UrlMappingDaoTest.java
@@ -0,0 +1,30 @@
+package kr.or.ddit.mvc.repository;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import kr.or.ddit.mvc.model.UrlMapping;
+
+public class UrlMappingDaoTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UrlMappingDaoTest.class);
+	
+	@Test
+	public void getUrlMappingTest() {
+		/***Given***/
+		IUrlMappingDao dao = new UrlMappingDao();
+
+		/***When***/
+		List<UrlMapping> list = dao.getUrlMapping();
+		
+		/***Then***/
+		assertEquals(2, list.size());
+		logger.debug("list : {}" , list);
+	}
+
+}
